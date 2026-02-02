df2ce03b293b6a12883558e0e98662afe658b06c
theajaypadigela <theajaypadigela@gmail.com>
2026-02-02 22:11:35 +0530

    simple dark-mode learning

--- App.tsx
+++ App.tsx
@@ -1,44 +1,46 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
-import { NewAppScreen } from '@react-native/new-app-screen';
-import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
-import {
-  SafeAreaProvider,
-  useSafeAreaInsets,
-} from 'react-native-safe-area-context';
+import React, { use } from 'react';
+import { View, Text, useColorScheme, StyleSheet } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 function App() {
   const isDarkMode = useColorScheme() === 'dark';
 
   return (
-    <SafeAreaProvider>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <AppContent />
-    </SafeAreaProvider>
-  );
-}
-
-function AppContent() {
-  const safeAreaInsets = useSafeAreaInsets();
-
-  return (
-    <View style={styles.container}>
-      <NewAppScreen
-        templateFileName="App.tsx"
-        safeAreaInsets={safeAreaInsets}
-      />
-    </View>
+    <SafeAreaView
+      style={isDarkMode ? styles.darkContainer : styles.lightContainer}
+    >
+      <View>
+        <Text style={isDarkMode ? styles.darkText : styles.lightText}>
+          Hello, World!
+        </Text>
+        <Text style={isDarkMode ? styles.darkText : styles.lightText}>
+          Hello, World!
+        </Text>
+        <Text style={isDarkMode ? styles.darkText : styles.lightText}>
+          Hello, World!
+        </Text>
+        <Text style={isDarkMode ? styles.darkText : styles.lightText}>
+          Hello, World!
+        </Text>
+      </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  darkContainer: {
+    backgroundColor: '#000000',
     flex: 1,
+  },
+  lightContainer: {
+    backgroundColor: '#FFFFFF',
+    flex: 1,
+  },
+  darkText: {
+    color: '#FFFFFF',
+  },
+  lightText: {
+    color: '#000000',
   },
 });
 
