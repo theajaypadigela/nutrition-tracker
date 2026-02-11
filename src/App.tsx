9c635103b6c51870691d6672b2a7f67485317149
theajaypadigela <theajaypadigela@gmail.com>
2026-02-11 10:52:38 +0530

    feat: Integrate NutritionReportScreen into the main app structure and define its macro/micro nutrient configuration constants.

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,31 @@
 import * as React from 'react';
-import { Text, View } from 'react-native';
-import { Button } from './components/ui';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { LoginScreen } from './components/LoginScreen';
+import RegisterScreen from './components/RegisterScreen';
+import { GluestackUIProvider } from './components/ui/gluestack-ui-provider';
+import DashBoardScreen from './components/DashBoardScreen';
+import AppBar from './components/AppBar';
+import BottomNavigation from './components/BottomNavigation';
+import HabitScreen from './components/HabitScreen';
+import ProfileScreen from './components/ProfileScreen';
+import FoodLogScreen from './components/FoodLogScreen';
+import NutritionReportScreen from './components/NutritionReportScreen';
 
 function App() {
   return (
-    <SafeAreaView style={{ flex: 1 }}>
-      <LoginScreen />
-    </SafeAreaView>
+    <GluestackUIProvider>
+      <SafeAreaView style={{ flex: 1 }}>
+        {/* <LoginScreen /> */}
+        {/* <RegisterScreen /> */}
+        <AppBar title="Habits" />
+        {/* <DashBoardScreen /> */}
+        {/* <HabitScreen></HabitScreen> */}
+        {/* <ProfileScreen name="Ajay" age="30" gender="Male"></ProfileScreen> */}
+        {/* <FoodLogScreen></FoodLogScreen> */}
+        <NutritionReportScreen></NutritionReportScreen>
+        <BottomNavigation activeTab="habits" onTabChange={() => {}} />
+      </SafeAreaView>
+    </GluestackUIProvider>
   );
 }
 
