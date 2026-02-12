03f0e2cf5b333e66ac95ea55faa0ede8758e2865
ChanduPinniboyena <pinniboyenachandu2018@gmail.com>
2026-02-12 19:38:10 +0530

    feat: Initialize Spring Boot backend project with basic Auth, FoodLogging, and Habits modules, and configure its pom.xml.

---
+++ backend/src/main/java/com/habitbuilder/habitbuilder/controller/FoodLogging.java
@@ -0,0 +1,10 @@
+package com.habitbuilder.habitbuilder.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/food-logging")
+public class FoodLogging {
+
+}
